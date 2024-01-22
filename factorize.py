002f3e06d3577a4f9b0ce02871e244106a02a5e6
Serghii777 <142201494+Serghii777@users.noreply.github.com>
2024-01-22 19:44:16 +0100

    commit

--- factorize.py
+++ factorize.py
@@ -18,6 +18,13 @@ if __name__ == '__main__':
     # Тестові дані
     test_numbers = (128, 255, 99999, 10651060)
 
+    a, b, c, d = test_numbers
+
+    assert factorize_single(a) == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert factorize_single(b) == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert factorize_single(c) == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert factorize_single(d) == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
     # Синхронна версія
     start_time = time.time()
     result_sync = factorize_sync(*test_numbers)
